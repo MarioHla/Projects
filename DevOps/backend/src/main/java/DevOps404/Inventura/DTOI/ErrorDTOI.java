bddd371f5bc22d9e11804e0c994b4385c23b3ed7
MarioHla <94127089+MarioHla@users.noreply.github.com>
2023-12-08 12:46:15 +0100

    Delete DevOps directory

--- DevOps/backend/src/main/java/DevOps404/Inventura/DTOI/ErrorDTOI.java
+++
@@ -1,9 +0,0 @@
-package DevOps404.Inventura.DTOI;
-
-import java.util.Date;
-
-public interface ErrorDTOI {
-
-	public void addError(Long badAmount, Long realAmount, Date time, Long werehouseId, Long articleId, Long workerId);
-	
-}
